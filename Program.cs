6b8791046addcd0c1f3456b17cec2e362a7e2e78
Annie Frank <annienishanthininz@gmail.com>
2022-08-10 21:28:12 +1200

    Initial commit -created starter project

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+
+builder.Services.AddSwaggerDocument(options =>
+{
+    options.DocumentName = "My Amazing API";
+    options.Version = "V1";
+
+});
+
+builder.Services.AddHttpClient("reddit", configureClient: client =>
+{
+    client.BaseAddress = new Uri("https://www.reddit.com/dev/api");
+});
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.UseOpenApi();
+app.UseSwaggerUi3();
+
+app.Run();
+
